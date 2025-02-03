dd7f1c71dafc5af2527a258afc3f43f1b353d095
Jack May <92079641+jackmay942149@users.noreply.github.com>
2025-02-03 13:37:31 +1000

    feat(char-select): added sprite changes

---
+++ Assets/scripts/ui/CharSelect.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class CharSelect : MonoBehaviour
+{
+    [SerializeField] Sprite[] sprites;
+    int index = 0;
+    SpriteRenderer sr;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    public void SpriteRight()
+    {
+        index++;
+        if (index >= sprites.Length) { index = 0; }
+        sr.sprite = sprites[index];
+    }
+
+    public void SpriteLeft()
+    {
+        index--;
+        if (index < 0) { index = sprites.Length - 1; }
+        sr.sprite = sprites[index];
+    }
+}
